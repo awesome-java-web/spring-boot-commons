52c4e7d73fdabe4c8ba15e92684a113040c86392
codeboyzhou <imzhouchen@gmail.com>
2024-04-22 00:42:06 +0800

    test(main): Add tests for LocalCacheManager

--- spring-boot-commons-scripting/src/test/java/com/github/springframework/boot/commons/groovy/GroovyScriptExecutorTest.java
+++ spring-boot-commons-scripting/src/test/java/com/github/springframework/boot/commons/groovy/GroovyScriptExecutorTest.java
@@ -1,11 +1,17 @@
 package com.github.springframework.boot.commons.groovy;
 
+import com.github.springframework.boot.commons.groovy.cache.CaffeineLocalCache;
+import com.github.springframework.boot.commons.groovy.cache.GuavaLocalCache;
 import com.github.springframework.boot.commons.groovy.cache.LocalCacheManager;
 import com.github.springframework.boot.commons.groovy.exception.GroovyObjectInvokeMethodException;
 import com.github.springframework.boot.commons.groovy.exception.GroovyScriptParseException;
 import com.github.springframework.boot.commons.groovy.exception.InvalidGroovyScriptException;
+import groovy.lang.GroovyObject;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.platform.commons.logging.Logger;
+import org.junit.platform.commons.logging.LoggerFactory;
+import org.mockito.Mockito;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -17,6 +23,8 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.junit.platform.commons.util.ClassLoaderUtils.getClassLoader;
 
 class GroovyScriptExecutorTest {
+
+	Logger logger = LoggerFactory.getLogger(GroovyScriptExecutorTest.class);
 
 	static GroovyScriptExecutor groovyScriptExecutor;
 
@@ -93,11 +101,62 @@ class GroovyScriptExecutorTest {
 
 	@Test
 	void testGroovyObjectInvokeMethodException() {
-		final String script = "def test() { throw new RuntimeException() }";
+		final String script = "def testAdd() { return 1 + 1 }";
 		GroovyObjectInvokeMethodException e = assertThrows(GroovyObjectInvokeMethodException.class,
 				() -> groovyScriptExecutor.evaluate(script, "test")
 		);
 		assertTrue(e.getMessage().contains("Failed to invoke groovy method"));
+	}
+
+	@Test
+	void testGroovySecurityInterceptor() {
+		// unsafe script case
+		final String unsafeScript1 = "def testSystemClassInterceptor() { System.gc() }";
+		GroovyObjectInvokeMethodException e = assertThrows(GroovyObjectInvokeMethodException.class,
+				() -> groovyScriptExecutor.evaluate(unsafeScript1, "testSystemClassInterceptor")
+		);
+		assertTrue(e.getMessage().contains("not allowed in your groovy code"));
+
+		// another unsafe script case
+		final String unsafeScript2 = "def testRuntimeClassInterceptor() { Runtime.getRuntime().gc() }";
+		e = assertThrows(GroovyObjectInvokeMethodException.class,
+				() -> groovyScriptExecutor.evaluate(unsafeScript2, "testRuntimeClassInterceptor")
+		);
+		assertTrue(e.getMessage().contains("not allowed in your groovy code"));
+
+		// safe script case
+		final String safeScript = "def testSystemClassInterceptor() { return System.currentTimeMillis() }";
+		Object result = groovyScriptExecutor.evaluate(safeScript, "testSystemClassInterceptor");
+		assertTrue(result instanceof Long && Long.parseLong(result.toString()) > 0);
+	}
+
+	@Test
+	void testLocalCacheManager() {
+		LocalCacheManager localCacheManager = LocalCacheManager.newBuilder();
+		GuavaLocalCache guava = new GuavaLocalCache(com.google.common.cache.CacheBuilder.newBuilder().build());
+		CaffeineLocalCache caffeine = new CaffeineLocalCache(com.github.benmanes.caffeine.cache.Caffeine.newBuilder().build());
+		GroovyObject mockGroovyObject = Mockito.mock(GroovyObject.class);
+
+		localCacheManager.use(guava);
+		localCacheManager.put("guava", mockGroovyObject);
+		assertNotNull(localCacheManager.getIfPresent("guava"));
+		assertNotNull(localCacheManager.stats());
+		logger.info(localCacheManager::stats);
+
+		localCacheManager.use(caffeine);
+		localCacheManager.put("caffeine", mockGroovyObject);
+		assertNotNull(localCacheManager.getIfPresent("caffeine"));
+		assertNotNull(localCacheManager.stats());
+		logger.info(localCacheManager::stats);
+
+		localCacheManager.useDefaultCache();
+		assertNotNull(localCacheManager.stats());
+		logger.info(localCacheManager::stats);
+
+		NullPointerException nullKey = assertThrows(NullPointerException.class, () -> localCacheManager.put(null, mockGroovyObject));
+		assertEquals("key == null || value == null", nullKey.getMessage());
+		NullPointerException nullVal = assertThrows(NullPointerException.class, () -> localCacheManager.put("lru", null));
+		assertEquals("key == null || value == null", nullVal.getMessage());
 	}
 
 }
