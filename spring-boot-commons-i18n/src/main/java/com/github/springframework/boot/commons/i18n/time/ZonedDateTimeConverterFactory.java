a0e0580f570dd7f9b652b8d63999332813a81c54
codeboyzhou <imzhouchen@gmail.com>
2024-04-24 15:54:25 +0800

    feat(main): Add spring-boot-commons-i18n dependency and modify ZonedDateTimeConverterFactory

--- spring-boot-commons-i18n/src/main/java/com/github/springframework/boot/commons/i18n/time/ZonedDateTimeConverterFactory.java
+++ spring-boot-commons-i18n/src/main/java/com/github/springframework/boot/commons/i18n/time/ZonedDateTimeConverterFactory.java
@@ -2,6 +2,7 @@ package com.github.springframework.boot.commons.i18n.time;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.util.Date;
 
 public final class ZonedDateTimeConverterFactory {
@@ -10,17 +11,25 @@ public final class ZonedDateTimeConverterFactory {
 		throw new UnsupportedOperationException("Factory class should not be instantiated");
 	}
 
-	@SuppressWarnings("rawtypes")
-	public static ZonedDateTimeConverter getConverter(Object datetime) {
-		if (datetime instanceof Date) {
-			return new JavaDateConverter();
-		} else if (datetime instanceof String) {
-			return new StringDateTimeConverter();
-		} else if (datetime instanceof LocalDate) {
-			return new LocalDateConverter();
-		} else if (datetime instanceof LocalDateTime) {
-			return new LocalDateTimeConverter();
+	public static Object convert(Object datetime, ZoneId targetZoneId) {
+		if (datetime == null) {
+			return null;
 		}
+
+		if (datetime instanceof Date) {
+			JavaDateConverter converter = new JavaDateConverter();
+			return converter.convert((Date) datetime, targetZoneId);
+		} else if (datetime instanceof String) {
+			StringDateTimeConverter converter = new StringDateTimeConverter();
+			return converter.convert(datetime.toString(), targetZoneId);
+		} else if (datetime instanceof LocalDate) {
+			LocalDateConverter localDateConverter = new LocalDateConverter();
+			return localDateConverter.convert((LocalDate) datetime, targetZoneId);
+		} else if (datetime instanceof LocalDateTime) {
+			LocalDateTimeConverter localDateTimeConverter = new LocalDateTimeConverter();
+			return localDateTimeConverter.convert((LocalDateTime) datetime, targetZoneId);
+		}
+
 		throw new UnsupportedOperationException("No suitable ZonedDateTimeConverter found to handle this data type: "
 				+ datetime.getClass() + ", you can report a issue on GitHub or consider to create a custom"
 				+ "ZonedDateTimeConverter implementation to support it.");
