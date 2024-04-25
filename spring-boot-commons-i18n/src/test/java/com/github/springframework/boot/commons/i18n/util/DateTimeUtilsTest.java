2f14b2e296fd357c4931ad96363c41c42f5b8255
codeboyzhou <imzhouchen@gmail.com>
2024-04-25 23:01:14 +0800

    test(main): Optimize test cases

--- spring-boot-commons-i18n/src/test/java/com/github/springframework/boot/commons/i18n/util/DateTimeUtilsTest.java
+++ spring-boot-commons-i18n/src/test/java/com/github/springframework/boot/commons/i18n/util/DateTimeUtilsTest.java
@@ -35,30 +35,39 @@ class DateTimeUtilsTest {
 			"2022-11-07 00:00:00, Asia/Phnom_Penh, 2022-11-06 23:00:00",
 			"2022-11-07 00:00:00, America/New_York, 2022-11-06 11:00:00"
 	})
-    void testToLocalDateTime(String datetimeAsDefaultZoneId, String targetZoneId, String expected) throws ParseException {
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern(DateTimeUtils.DEFAULT_DATE_TIME_PATTERN);
-        ZoneId zoneId = ZoneId.of(targetZoneId);
+    void testToLocalDateTime(String datetimeWithDefaultZone, String targetZoneId, String expected) throws ParseException {
+		ZoneId zoneId = ZoneId.of(targetZoneId);
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern(DateTimeUtils.DEFAULT_DATE_TIME_PATTERN);
 
-        LocalDateTime result = DateTimeUtils.toLocalDateTime(datetimeAsDefaultZoneId, formatter, zoneId);
+        LocalDateTime result = DateTimeUtils.toLocalDateTime(datetimeWithDefaultZone, formatter, zoneId);
         assertEquals(expected, result.format(formatter));
 
-        SimpleDateFormat sdf = new SimpleDateFormat(DateTimeUtils.DEFAULT_DATE_TIME_PATTERN);
-        Date parsedDate = sdf.parse(datetimeAsDefaultZoneId);
+        SimpleDateFormat dateFormat = new SimpleDateFormat(DateTimeUtils.DEFAULT_DATE_TIME_PATTERN);
+        Date parsedDate = dateFormat.parse(datetimeWithDefaultZone);
         result = DateTimeUtils.toLocalDateTime(parsedDate, zoneId);
         assertEquals(expected, result.format(formatter));
 
-        LocalDateTime localDateTime = LocalDateTime.parse(datetimeAsDefaultZoneId, formatter);
+        LocalDateTime localDateTime = LocalDateTime.parse(datetimeWithDefaultZone, formatter);
         result = DateTimeUtils.toLocalDateTime(localDateTime, zoneId);
         assertEquals(expected, result.format(formatter));
 
         LocalDate localDate = localDateTime.toLocalDate();
-        LocalDate zonedLocalDate = DateTimeUtils.toLocalDate(localDate, zoneId);
+		LocalDate zonedLocalDate = DateTimeUtils.toLocalDate(localDate, zoneId);
         assertEquals(expected.substring(0, 10), zonedLocalDate.toString());
 
         Date date = DateTimeUtils.toDate(localDateTime);
         result = DateTimeUtils.toLocalDateTime(date, zoneId);
         assertEquals(expected, result.format(formatter));
     }
+
+	@ParameterizedTest
+	@CsvSource({"2022-11-07, Asia/Phnom_Penh, 2022-11-06", "2022-11-07, America/New_York, 2022-11-06"})
+	void testToLocalDate(String datetimeWithDefaultZone, String targetZoneId, String expected) {
+		ZoneId zoneId = ZoneId.of(targetZoneId);
+		DateTimeFormatter formatter = DateTimeFormatter.ISO_LOCAL_DATE;
+		LocalDate localDate = DateTimeUtils.toLocalDate(datetimeWithDefaultZone, formatter, zoneId);
+		assertEquals(expected, localDate.format(formatter));
+	}
 
     @ParameterizedTest
     @CsvSource({"2022-11-07 00:00:00, true", "This is not a LocalDateTime string, false"})
--- spring-boot-commons-util/src/test/java/com/github/springframework/boot/commons/util/MessageDigestUtilsTest.java
+++ spring-boot-commons-util/src/test/java/com/github/springframework/boot/commons/util/MessageDigestUtilsTest.java
@@ -3,9 +3,15 @@ package com.github.springframework.boot.commons.util;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.mockito.MockedStatic;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.mockStatic;
 
 class MessageDigestUtilsTest {
 
@@ -23,6 +29,14 @@ class MessageDigestUtilsTest {
 	})
 	void testMd5Hex(String input, String expected) {
 		assertEquals(expected, MessageDigestUtils.md5Hex(input));
+	}
+
+	@Test
+	void testNoSuchAlgorithmException() {
+		MockedStatic<MessageDigest> mockMessageDigest = mockStatic(MessageDigest.class);
+		mockMessageDigest.when(() -> MessageDigest.getInstance(anyString())).thenThrow(new NoSuchAlgorithmException());
+		assertEquals(0, MessageDigestUtils.md5(anyString()).length);
+		mockMessageDigest.close();
 	}
 
 }
