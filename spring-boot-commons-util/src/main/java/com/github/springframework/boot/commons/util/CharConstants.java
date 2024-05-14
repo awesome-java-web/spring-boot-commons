2b5f62e11797ebd8581f08a5ee3af91070f6f463
codeboyzhou <imzhouchen@gmail.com>
2024-05-14 13:51:36 +0700

    feat(main): Add CharConstants enum

---
+++ spring-boot-commons-util/src/main/java/com/github/springframework/boot/commons/util/CharConstants.java
@@ -0,0 +1,32 @@
+package com.github.springframework.boot.commons.util;
+
+public enum CharConstants {
+
+    AT('@'),
+
+    DOT('.'),
+
+    DASH('-'),
+
+    COMMA(','),
+
+    ;
+
+    private final char charValue;
+
+    private final String stringValue;
+
+    CharConstants(char charValue) {
+        this.charValue = charValue;
+        this.stringValue = String.valueOf(charValue);
+    }
+
+    public char charValue() {
+        return charValue;
+    }
+
+    public String stringValue() {
+        return stringValue;
+    }
+
+}
