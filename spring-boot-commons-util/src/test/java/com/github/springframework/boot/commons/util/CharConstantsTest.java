637a2568d6d93097b6d51b50ec6c213138c6faac
codeboyzhou <imzhouchen@gmail.com>
2024-07-30 15:31:58 +0800

    test(1.0.x): Add tests for CharConstants

---
+++ spring-boot-commons-util/src/test/java/com/github/springframework/boot/commons/util/CharConstantsTest.java
@@ -0,0 +1,24 @@
+package com.github.springframework.boot.commons.util;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class CharConstantsTest {
+
+    @Test
+    void testCharConstants() {
+        assertEquals('@', CharConstants.AT.charValue());
+        assertEquals('.', CharConstants.DOT.charValue());
+        assertEquals('-', CharConstants.DASH.charValue());
+        assertEquals(',', CharConstants.COMMA.charValue());
+        assertEquals('_', CharConstants.UNDERLINE.charValue());
+
+        assertEquals("@", CharConstants.AT.stringValue());
+        assertEquals(".", CharConstants.DOT.stringValue());
+        assertEquals("-", CharConstants.DASH.stringValue());
+        assertEquals(",", CharConstants.COMMA.stringValue());
+        assertEquals("_", CharConstants.UNDERLINE.stringValue());
+    }
+
+}
