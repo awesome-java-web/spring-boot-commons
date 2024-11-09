2f3bdc9b74a48449ce0ee28a09ab1e17192f803a
codeboyzhou <imzhouchen@gmail.com>
2024-11-09 12:11:21 +0800

    refactor(1.0.x): AbstractRetryHandler类方法重构

--- spring-boot-commons-retry/src/main/java/com/github/springframework/boot/commons/retry/AbstractRetryHandler.java
+++ spring-boot-commons-retry/src/main/java/com/github/springframework/boot/commons/retry/AbstractRetryHandler.java
@@ -4,11 +4,33 @@ import java.util.concurrent.TimeUnit;
 
 public abstract class AbstractRetryHandler implements RetryHandler {
 
-	private static final int DEFAULT_MAX_ATTEMPTS = 3;
+	protected static final int DEFAULT_MAX_ATTEMPTS = 3;
 
 	protected int maxAttempts = DEFAULT_MAX_ATTEMPTS;
 
-	protected void sleepQuietly(long milliseconds) {
+	protected final ThreadLocal<Integer> attempts = ThreadLocal.withInitial(() -> 0);
+
+	public abstract long getInterval();
+
+	@Override
+	public void retry(RetryCallback callback) {
+		while (attempts.get() < maxAttempts) {
+			final long interval = getInterval();
+			sleepQuietly(interval);
+			if (callback.execute()) {
+				break;
+			}
+			attempts.set(attempts.get() + 1);
+		}
+
+		attempts.remove();
+
+		if (attempts.get() == maxAttempts) {
+			throw new RetryFailedException("Reached max attempts: " + maxAttempts);
+		}
+	}
+
+	private void sleepQuietly(long milliseconds) {
 		try {
 			TimeUnit.MILLISECONDS.sleep(milliseconds);
 		} catch (InterruptedException e) {
