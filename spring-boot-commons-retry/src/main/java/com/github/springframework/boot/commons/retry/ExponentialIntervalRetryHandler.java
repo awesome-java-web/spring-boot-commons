a6cce08c7d83fcd01893c8bd5aa1b855fd8a914f
codeboyzhou <imzhouchen@gmail.com>
2024-11-09 10:55:18 +0800

    feat(1.0.x): 新增[指数递增时间间隔]重试策略

---
+++ spring-boot-commons-retry/src/main/java/com/github/springframework/boot/commons/retry/ExponentialIntervalRetryHandler.java
@@ -0,0 +1,45 @@
+package com.github.springframework.boot.commons.retry;
+
+import java.util.concurrent.TimeUnit;
+
+public class ExponentialIntervalRetryHandler extends AbstractRetryHandler {
+
+	private static final long DEFAULT_INITIAL_INTERVAL = TimeUnit.SECONDS.toMillis(3);
+
+	private static final long DEFAULT_MAX_INTERVAL = TimeUnit.SECONDS.toMillis(30);
+
+	protected long initialInterval;
+
+	protected long maxInterval;
+
+	public static ExponentialIntervalRetryHandler newHandler() {
+		return new ExponentialIntervalRetryHandler();
+	}
+
+	public ExponentialIntervalRetryHandler withInitialInterval(long initialInterval){
+		this.initialInterval = initialInterval;
+		return this;
+	}
+
+	public ExponentialIntervalRetryHandler withMaxInterval(long maxInterval){
+		this.maxInterval = maxInterval;
+		return this;
+	}
+
+	public ExponentialIntervalRetryHandler withMaxAttempts(int maxAttempts) {
+		this.maxAttempts = maxAttempts;
+		return this;
+	}
+
+	public ExponentialIntervalRetryHandler() {
+		this.initialInterval = DEFAULT_INITIAL_INTERVAL;
+		this.maxInterval = DEFAULT_MAX_INTERVAL;
+	}
+
+	@Override
+	public long getInterval() {
+		final int multiplier = 1 << attempts.get();
+		return Math.min(initialInterval * multiplier, maxInterval);
+	}
+
+}
