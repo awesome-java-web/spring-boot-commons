00ab37c0e6baa9232e424d346550e9da33848305
codeboyzhou <imzhouchen@gmail.com>
2024-11-09 14:54:24 +0800

    refactor(1.0.x): RedisTemplateHelper#setIfAbsent()方法逻辑简化

--- spring-boot-commons-util/src/main/java/com/github/springframework/boot/commons/util/RedisTemplateHelper.java
+++ spring-boot-commons-util/src/main/java/com/github/springframework/boot/commons/util/RedisTemplateHelper.java
@@ -1,10 +1,10 @@
 package com.github.springframework.boot.commons.util;
 
-import org.springframework.data.redis.connection.RedisScriptingCommands;
-import org.springframework.data.redis.connection.ReturnType;
-import org.springframework.data.redis.core.RedisCallback;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.script.DefaultRedisScript;
+import org.springframework.data.redis.core.script.RedisScript;
 
+import java.util.Collections;
 import java.util.concurrent.TimeUnit;
 
 public final class RedisTemplateHelper {
@@ -16,14 +16,8 @@ public final class RedisTemplateHelper {
 	public static boolean setIfAbsent(StringRedisTemplate redisTemplate, String key, String value, long expiration, TimeUnit unit) {
 		final String luaScript = "return redis.call('SET', KEYS[1], ARGV[1], 'NX', 'EX', ARGV[2]) and true or false";
 		final String seconds = String.valueOf(unit.toSeconds(expiration));
-		final Boolean result = redisTemplate.execute(
-			(RedisCallback<Boolean>) connection -> {
-				RedisScriptingCommands redisScriptingCommands = connection.scriptingCommands();
-				return redisScriptingCommands.eval(
-					luaScript.getBytes(), ReturnType.BOOLEAN, 1, key.getBytes(), value.getBytes(), seconds.getBytes()
-				);
-			}
-		);
+		RedisScript<Boolean> redisScript = new DefaultRedisScript<>(luaScript, Boolean.class);
+		Boolean result = redisTemplate.execute(redisScript, Collections.singletonList(key), value, seconds);
 		return Boolean.TRUE.equals(result);
 	}
 
