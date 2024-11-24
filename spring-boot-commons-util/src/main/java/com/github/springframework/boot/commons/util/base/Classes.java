bc7f4de718d37a9f80bb7a85286ca8841ebf47e4
codeboyzhou <imzhouchen@gmail.com>
2024-11-24 12:45:45 +0800

    performance(1.0.x): Classes.getAllFieldsOf()方法使用缓存减少反射性能开销

--- spring-boot-commons-util/src/main/java/com/github/springframework/boot/commons/util/base/Classes.java
+++ spring-boot-commons-util/src/main/java/com/github/springframework/boot/commons/util/base/Classes.java
@@ -7,17 +7,24 @@ import java.util.List;
 
 public final class Classes {
 
+	private static final LRUMap<String, List<Field>> CLASS_FIELDS_CACHE = new LRUMap<>(100);
+
 	public Classes() {
 		throw new UnsupportedOperationException("Utility class should not be instantiated");
 	}
 
 	public static List<Field> getAllFieldsOf(Class<?> clazz) {
+		List<Field> cachedFields = CLASS_FIELDS_CACHE.get(clazz.getName());
+		if (cachedFields != null) {
+			return cachedFields;
+		}
 		List<Field> allFields = new ArrayList<>();
 		while (clazz != Object.class) {
 			Field[] declaredFields = clazz.getDeclaredFields();
 			allFields.addAll(Arrays.asList(declaredFields));
 			clazz = clazz.getSuperclass();
 		}
+		CLASS_FIELDS_CACHE.put(clazz.getName(), allFields);
 		return allFields;
 	}
 
