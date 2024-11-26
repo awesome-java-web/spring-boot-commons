7aa41b936cda87e93c909332683ce6fcf34e315d
codeboyzhou <imzhouchen@gmail.com>
2024-11-26 19:57:34 +0800

    fix(1.0.x): 修复Classes.getAllFieldsOf()方法中缓存put操作的逻辑错误

--- spring-boot-commons-util/src/main/java/com/github/springframework/boot/commons/util/base/Classes.java
+++ spring-boot-commons-util/src/main/java/com/github/springframework/boot/commons/util/base/Classes.java
@@ -7,33 +7,34 @@ import java.util.List;
 
 public final class Classes {
 
-	private static final LRUMap<String, List<Field>> CLASS_FIELDS_CACHE = new LRUMap<>(100);
+    private static final LRUMap<String, List<Field>> CLASS_FIELDS_CACHE = new LRUMap<>(100);
 
-	public Classes() {
-		throw new UnsupportedOperationException("Utility class should not be instantiated");
-	}
+    public Classes() {
+        throw new UnsupportedOperationException("Utility class should not be instantiated");
+    }
 
-	public static List<Field> getAllFieldsOf(Class<?> clazz) {
-		List<Field> cachedFields = CLASS_FIELDS_CACHE.get(clazz.getName());
-		if (cachedFields != null) {
-			return cachedFields;
-		}
-		List<Field> allFields = new ArrayList<>();
-		while (clazz != Object.class) {
-			Field[] declaredFields = clazz.getDeclaredFields();
-			allFields.addAll(Arrays.asList(declaredFields));
-			clazz = clazz.getSuperclass();
-		}
-		CLASS_FIELDS_CACHE.put(clazz.getName(), allFields);
-		return allFields;
-	}
+    public static List<Field> getAllFieldsOf(Class<?> clazz) {
+        final String className = clazz.getName();
+        List<Field> cachedFields = CLASS_FIELDS_CACHE.get(className);
+        if (cachedFields != null) {
+            return cachedFields;
+        }
+        List<Field> allFields = new ArrayList<>();
+        while (clazz != Object.class) {
+            Field[] declaredFields = clazz.getDeclaredFields();
+            allFields.addAll(Arrays.asList(declaredFields));
+            CLASS_FIELDS_CACHE.put(className, allFields);
+            clazz = clazz.getSuperclass();
+        }
+        return allFields;
+    }
 
-	public static Object newInstanceOf(Class<?> clazz) {
-		try {
-			return clazz.newInstance();
-		} catch (InstantiationException | IllegalAccessException e) {
-			return null;
-		}
-	}
+    public static Object newInstanceOf(Class<?> clazz) {
+        try {
+            return clazz.newInstance();
+        } catch (InstantiationException | IllegalAccessException e) {
+            return null;
+        }
+    }
 
 }
