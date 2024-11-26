6f7af0196f75cbccf736b0100c2b1eb603819d73
codeboyzhou <imzhouchen@gmail.com>
2024-11-27 01:58:51 +0800

    refactor(1.0.x): 在spring-boot-commons-all模块中统一管理自动配置类

--- spring-boot-commons-all/src/main/java/com/github/springframework/boot/commons/BeanInitializer.java
+++ spring-boot-commons-all/src/main/java/com/github/springframework/boot/commons/BeanInitializer.java
@@ -1,16 +1,22 @@
-package com.github.springframework.boot.commons.web;
+package com.github.springframework.boot.commons;
 
+import com.github.springframework.boot.commons.groovy.GroovyScriptExecutor;
 import com.github.springframework.boot.commons.web.interceptor.DuplicateHttpRequestInterceptor;
 import com.github.springframework.boot.commons.web.mail.MailSendService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class AutoConfiguration {
+public class BeanInitializer {
 
     @Bean
     public MailSendService mailSender() {
         return new MailSendService();
+    }
+
+    @Bean
+    public GroovyScriptExecutor groovyScriptExecutor() {
+        return new GroovyScriptExecutor();
     }
 
     @Bean
