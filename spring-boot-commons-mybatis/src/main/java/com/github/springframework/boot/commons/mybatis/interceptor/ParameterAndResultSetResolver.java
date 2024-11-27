f45c881ef11003b4e6aa31028f614bf9faa3a2c5
codeboyzhou <imzhouchen@gmail.com>
2024-11-27 18:00:21 +0800

    refactor(1.0.x): 移除Classes工具类使用spring框架自带的ReflectionUtils类

--- spring-boot-commons-mybatis/src/main/java/com/github/springframework/boot/commons/mybatis/interceptor/ParameterAndResultSetResolver.java
+++ spring-boot-commons-mybatis/src/main/java/com/github/springframework/boot/commons/mybatis/interceptor/ParameterAndResultSetResolver.java
@@ -2,142 +2,132 @@ package com.github.springframework.boot.commons.mybatis.interceptor;
 
 import com.github.springframework.boot.commons.mybatis.handler.MybatisFieldHandler;
 import com.github.springframework.boot.commons.mybatis.util.FieldNameUtils;
-import com.github.springframework.boot.commons.util.base.Classes;
 import org.apache.ibatis.binding.MapperMethod;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.util.ReflectionUtils;
 
-import java.lang.reflect.Field;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
-import static java.util.stream.Collectors.toMap;
-
 class ParameterAndResultSetResolver {
 
-	private static final Logger logger = LoggerFactory.getLogger(ParameterAndResultSetResolver.class);
+    private static final Logger logger = LoggerFactory.getLogger(ParameterAndResultSetResolver.class);
 
-	@SuppressWarnings("unchecked")
-	public void resolve(MybatisFieldHandler handler, final String tableName, Object value) throws IllegalAccessException {
-		if (value instanceof Map) {
-			resolveMap(handler, tableName, (Map<String, Object>) value);
-		} else if (value instanceof Iterable) {
-			resolveIterable(handler, tableName, (Iterable<?>) value);
-		} else if (isUserDefinedObject(value)) {
-			resolveUserDefinedObject(handler, tableName, value);
-		}
-	}
+    @SuppressWarnings("unchecked")
+    public void resolve(MybatisFieldHandler handler, final String tableName, Object value) throws IllegalAccessException {
+        if (value instanceof Map) {
+            resolveMap(handler, tableName, (Map<String, Object>) value);
+        } else if (value instanceof Iterable) {
+            resolveIterable(handler, tableName, (Iterable<?>) value);
+        } else if (isUserDefinedObject(value)) {
+            resolveUserDefinedObject(handler, tableName, value);
+        }
+    }
 
-	private void resolveMap(MybatisFieldHandler handler, final String tableName, Map<String, Object> map) throws IllegalAccessException {
-		// If map instanceof Map<K, Iterable<E>>
-		final boolean mapValueIterable = map.values().iterator().next() instanceof Iterable;
-		if (mapValueIterable) {
-			if (logger.isDebugEnabled()) {
-				logger.debug("The map.values() is iterable for table '{}', use 'handleIterable' method to proceed", tableName);
-			}
-			// If the map instanceof org.apache.ibatis.binding.MapperMethod.ParamMap, there could be
-			// multiple same values but with different keys in the map, so we need to get distinct
-			// values to avoid duplicate handling.
-			final boolean isMybatisParamMap = map instanceof MapperMethod.ParamMap;
-			Set<Object> distinctValues = Collections.singleton(map.values().iterator().next());
-			resolveIterable(handler, tableName, isMybatisParamMap ? distinctValues : map.values());
-		}
-		// If map instanceof Map<K, V>
-		else {
-			Map<String, List<String>> targetTableFields = handler.targetTableFields();
-			List<String> tableFieldNames = FieldNameUtils.determineTargetFieldNames(targetTableFields, tableName);
-			if (logger.isDebugEnabled()) {
-				logger.debug("The map.values() is not iterable for table '{}', will be handled as typical Map<K,V> with target table fields {}",
-					tableName, tableFieldNames
-				);
-			}
+    private void resolveMap(MybatisFieldHandler handler, final String tableName, Map<String, Object> map) throws IllegalAccessException {
+        // If map instanceof Map<K, Iterable<E>>
+        final boolean mapValueIterable = map.values().iterator().next() instanceof Iterable;
+        if (mapValueIterable) {
+            if (logger.isDebugEnabled()) {
+                logger.debug("The map.values() is iterable for table '{}', use 'handleIterable' method to proceed", tableName);
+            }
+            // If the map instanceof org.apache.ibatis.binding.MapperMethod.ParamMap, there could be
+            // multiple same values but with different keys in the map, so we need to get distinct
+            // values to avoid duplicate handling.
+            final boolean isMybatisParamMap = map instanceof MapperMethod.ParamMap;
+            Set<Object> distinctValues = Collections.singleton(map.values().iterator().next());
+            resolveIterable(handler, tableName, isMybatisParamMap ? distinctValues : map.values());
+        }
+        // If map instanceof Map<K, V>
+        else {
+            Map<String, List<String>> targetTableFields = handler.targetTableFields();
+            List<String> tableFieldNames = FieldNameUtils.determineTargetFieldNames(targetTableFields, tableName);
+            if (logger.isDebugEnabled()) {
+                logger.debug("The map.values() is not iterable for table '{}', will be handled as typical Map<K,V> with target table fields {}",
+                    tableName, tableFieldNames
+                );
+            }
 
-			int tableFieldNameMatches = 0;
-			for (String tableFieldName : tableFieldNames) {
-				final String fieldCamelCaseName = FieldNameUtils.underscoreToCamelCase(tableFieldName);
-				// Do not use map.get() method directly here, because if the map instanceof
-				// org.apache.ibatis.binding.MapperMethod.ParamMap, it will throw an exception.
-				if (map.containsKey(fieldCamelCaseName)) {
-					Object mapValue = map.get(fieldCamelCaseName);
-					Object handledValue = handler.handle(tableName, tableFieldName, mapValue);
-					map.put(fieldCamelCaseName, handledValue);
-					tableFieldNameMatches++;
-				}
-			}
+            int tableFieldNameMatches = 0;
+            for (String tableFieldName : tableFieldNames) {
+                final String fieldCamelCaseName = FieldNameUtils.underscoreToCamelCase(tableFieldName);
+                // Do not use map.get() method directly here, because if the map instanceof
+                // org.apache.ibatis.binding.MapperMethod.ParamMap, it will throw an exception.
+                if (map.containsKey(fieldCamelCaseName)) {
+                    Object mapValue = map.get(fieldCamelCaseName);
+                    Object handledValue = handler.handle(tableName, tableFieldName, mapValue);
+                    map.put(fieldCamelCaseName, handledValue);
+                    tableFieldNameMatches++;
+                }
+            }
 
-			// If no table field name matches, it means the map is not a typical table field map.
-			if (tableFieldNameMatches == 0) {
-				Object userDefinedObject = map.values().iterator().next();
-				resolveUserDefinedObject(handler, tableName, userDefinedObject);
-			}
-		}
-	}
+            // If no table field name matches, it means the map is not a typical table field map.
+            if (tableFieldNameMatches == 0) {
+                Object userDefinedObject = map.values().iterator().next();
+                resolveUserDefinedObject(handler, tableName, userDefinedObject);
+            }
+        }
+    }
 
-	@SuppressWarnings("unchecked")
-	private void resolveIterable(MybatisFieldHandler handler, final String tableName, Iterable<?> iterable) throws IllegalAccessException {
-		for (Object element : iterable) {
-			if (element instanceof Map) {
-				if (logger.isDebugEnabled()) {
-					logger.debug("Ths inner element is instance of Map, use 'resolveMap' method to proceed");
-				}
-				resolveMap(handler, tableName, (Map<String, Object>) element);
-			} else if (element instanceof Iterable) {
-				if (logger.isDebugEnabled()) {
-					logger.debug("Ths inner element is instance of Iterable, use 'resolveIterable' method to proceed");
-				}
-				resolveIterable(handler, tableName, (Iterable<?>) element);
-			} else if (isUserDefinedObject(element)) {
-				if (logger.isDebugEnabled()) {
-					logger.debug("The inner element is user defined Java object, use 'resolveUserDefinedObject' method to proceed");
-				}
-				resolveUserDefinedObject(handler, tableName, element);
-			}
-		}
-	}
+    @SuppressWarnings("unchecked")
+    private void resolveIterable(MybatisFieldHandler handler, final String tableName, Iterable<?> iterable) throws IllegalAccessException {
+        for (Object element : iterable) {
+            if (element instanceof Map) {
+                if (logger.isDebugEnabled()) {
+                    logger.debug("Ths inner element is instance of Map, use 'resolveMap' method to proceed");
+                }
+                resolveMap(handler, tableName, (Map<String, Object>) element);
+            } else if (element instanceof Iterable) {
+                if (logger.isDebugEnabled()) {
+                    logger.debug("Ths inner element is instance of Iterable, use 'resolveIterable' method to proceed");
+                }
+                resolveIterable(handler, tableName, (Iterable<?>) element);
+            } else if (isUserDefinedObject(element)) {
+                if (logger.isDebugEnabled()) {
+                    logger.debug("The inner element is user defined Java object, use 'resolveUserDefinedObject' method to proceed");
+                }
+                resolveUserDefinedObject(handler, tableName, element);
+            }
+        }
+    }
 
-	private void resolveUserDefinedObject(MybatisFieldHandler handler, final String tableName, Object object) throws IllegalAccessException {
-		Map<String, List<String>> targetTableFields = handler.targetTableFields();
-		List<String> tableFieldNames = FieldNameUtils.determineTargetFieldNames(targetTableFields, tableName);
+    private void resolveUserDefinedObject(MybatisFieldHandler handler, final String tableName, Object object) throws IllegalAccessException {
+        Map<String, List<String>> targetTableFields = handler.targetTableFields();
+        List<String> tableFieldNames = FieldNameUtils.determineTargetFieldNames(targetTableFields, tableName);
 
-		if (logger.isDebugEnabled()) {
-			logger.debug("Fields {} of table '{}' will be handled for user defined Java object {}", tableFieldNames, tableName, object);
-		}
+        if (logger.isDebugEnabled()) {
+            logger.debug("Fields {} of table '{}' will be handled for user defined Java object {}", tableFieldNames, tableName, object);
+        }
 
-		if (tableFieldNames == null || tableFieldNames.isEmpty()) {
-			if (logger.isDebugEnabled()) {
-				logger.debug("No fields need to be handled for table '{}', skip to proceed", tableName);
-			}
-			return;
-		}
+        if (tableFieldNames == null || tableFieldNames.isEmpty()) {
+            if (logger.isDebugEnabled()) {
+                logger.debug("No fields need to be handled for table '{}', skip to proceed", tableName);
+            }
+            return;
+        }
 
-		List<Field> javaFields = Classes.getAllFieldsOf(object.getClass());
-		Map<String, Field> javaFieldMap = javaFields.stream().collect(
-			toMap(Field::getName, field -> field, (existing, replacement) -> existing)
-		);
-		for (String tableFieldName : tableFieldNames) {
-			final String javaFieldName = FieldNameUtils.underscoreToCamelCase(tableFieldName);
-			Field javaField = javaFieldMap.get(javaFieldName);
-			if (javaField == null) {
-				if (logger.isDebugEnabled()) {
-					logger.debug("Java field '{}' is not found in object {}, skip to proceed", javaFieldName, object);
-				}
-				continue;
-			}
-			javaField.setAccessible(true);
-			Object javaFieldValue = javaField.get(object);
-			Object handledValue = handler.handle(tableName, tableFieldName, javaFieldValue);
-			javaField.set(object, handledValue);
-		}
-	}
+        ReflectionUtils.doWithFields(object.getClass(), field -> {
+            for (String tableFieldName : tableFieldNames) {
+                final String javaFieldName = FieldNameUtils.underscoreToCamelCase(tableFieldName);
+                if (javaFieldName.equals(field.getName())) {
+                    field.setAccessible(true);
+                    Object javaFieldValue = field.get(object);
+                    Object handledValue = handler.handle(tableName, tableFieldName, javaFieldValue);
+                    field.set(object, handledValue);
+                }
+            }
+        });
+    }
 
-	private static boolean isUserDefinedObject(Object object) {
-		if (object == null) {
-			return false;
-		}
-		final String packageName = object.getClass().getPackage().getName();
-		return !packageName.startsWith("java.") && !packageName.startsWith("javax.") && !packageName.startsWith("com.sun.");
-	}
+    private static boolean isUserDefinedObject(Object object) {
+        if (object == null) {
+            return false;
+        }
+        final String packageName = object.getClass().getPackage().getName();
+        return !packageName.startsWith("java.") && !packageName.startsWith("javax.") && !packageName.startsWith("com.sun.");
+    }
 
 }
--- spring-boot-commons-web/src/main/java/com/github/springframework/boot/commons/web/interceptor/DuplicateHttpRequestInterceptor.java
+++ spring-boot-commons-web/src/main/java/com/github/springframework/boot/commons/web/interceptor/DuplicateHttpRequestInterceptor.java
@@ -1,10 +1,9 @@
 package com.github.springframework.boot.commons.web.interceptor;
 
-import com.github.springframework.boot.commons.util.base.Classes;
 import com.github.springframework.boot.commons.web.annotation.DuplicateRequestConstraint;
-import com.github.springframework.boot.commons.web.exception.DuplicateRequestConstraintException;
 import com.github.springframework.boot.commons.web.request.DuplicateRequestConstraintStrategy;
 import org.jetbrains.annotations.NotNull;
+import org.springframework.data.util.ReflectionUtils;
 import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.HandlerInterceptor;
 
@@ -14,25 +13,22 @@ import java.lang.reflect.Method;
 
 public class DuplicateHttpRequestInterceptor implements HandlerInterceptor {
 
-	@Override
-	public boolean preHandle(@NotNull HttpServletRequest request, @NotNull HttpServletResponse response, @NotNull Object handler) {
-		if (handler instanceof HandlerMethod) {
-			HandlerMethod handlerMethod = (HandlerMethod) handler;
-			Method method = handlerMethod.getMethod();
+    @Override
+    public boolean preHandle(@NotNull HttpServletRequest request, @NotNull HttpServletResponse response, @NotNull Object handler) {
+        if (handler instanceof HandlerMethod) {
+            HandlerMethod handlerMethod = (HandlerMethod) handler;
+            Method method = handlerMethod.getMethod();
 
-			DuplicateRequestConstraint constraint = method.getAnnotation(DuplicateRequestConstraint.class);
-			if (constraint == null || constraint.strategy() == null) {
-				return true;
-			}
+            DuplicateRequestConstraint constraint = method.getAnnotation(DuplicateRequestConstraint.class);
+            if (constraint == null || constraint.strategy() == null) {
+                return true;
+            }
 
-			Object strategyObject = Classes.newInstanceOf(constraint.strategy());
-			if (strategyObject == null) {
-				throw new DuplicateRequestConstraintException("Failed to create an instance of DuplicateRequestConstraintStrategy");
-			}
-			DuplicateRequestConstraintStrategy strategy = (DuplicateRequestConstraintStrategy) strategyObject;
-			return strategy.proceed(request, constraint);
-		}
-		return true;
-	}
+            Object strategyObject = ReflectionUtils.createInstanceIfPresent(constraint.strategy().getName(), new Object());
+            DuplicateRequestConstraintStrategy strategy = (DuplicateRequestConstraintStrategy) strategyObject;
+            return strategy.proceed(request, constraint);
+        }
+        return true;
+    }
 
 }
